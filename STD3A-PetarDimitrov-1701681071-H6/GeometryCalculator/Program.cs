04212cd99e74b4975cd368bbe1d24d44112f31f8
PetarDimitrov <petio5678@abv.bg>
2017-11-03 10:31:13 +0200

    Initial commit.

---
+++ STD3A-PetarDimitrov-1701681071-H6/GeometryCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu();
+            Console.ReadKey(true);
+        }
+
+        public static void Square()
+        {
+            Console.WriteLine("Enter side:");
+            float side = float.Parse(Console.ReadLine());
+            Console.WriteLine("The Area of the Square is:{0:f2}", side * side);
+            Menu();
+        }
+        public static void Triangle()
+        {
+            Console.WriteLine("Enter side: ");
+            float SideOfTriangle = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("The Area of the Triangle is:{0:f2}", (SideOfTriangle * height) / 2);
+            Menu();
+        }
+        public static void Radians()
+        {
+            string radians = Console.ReadLine();
+            Console.WriteLine("Enter the degrees you want to transform in radians:");
+            float degrees = float.Parse(Console.ReadLine());
+            double Radians = degrees * Math.PI / 180;
+            Console.WriteLine("Your degrees in radians are = {0}", Radians);
+            Menu();
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("Choose figure(square,triangle or radians):");
+            string figures = Console.ReadLine();
+            switch (figures)
+            {
+                case "square":
+                    Square();
+                    break;
+                case "triangle":
+                    Triangle();
+                    break;
+                case "radians":
+                    Radians();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ STD3A-PetarDimitrov-1701681071-H6/GetMax/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The biggest number is:{0}", Max(firstNumber, secondNumber, thirdNumber));
+        }
+        public static int Max(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int max = firstNumber < secondNumber ? (secondNumber < thirdNumber ? thirdNumber : secondNumber) : (firstNumber < thirdNumber ? thirdNumber : firstNumber);
+            return Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber));
+
+
+        }
+
+    }
+}
